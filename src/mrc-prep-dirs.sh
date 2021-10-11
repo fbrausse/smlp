543e02220a1f66e67b58c8341f174ee67d70ee37
Franz Brauße <franz.brausse@intel.com>
2021-10-11 10:11:40 +0200

    mrc-prep-dirs: rename CH to MC

--- src/mrc-prep-dirs.sh
+++ src/mrc-prep-dirs.sh
@@ -14,7 +14,7 @@ export PATH="${bin}:${PATH}"
 [[ $# -ge 1 ]] && cd "$1"
 ${bin}/split-categorical2.py -f RANK data.spec data.csv rank &&
 (cd rank0 &&
- ${bin}/split-categorical2.py -f CH data.spec data.csv ch &&
+ ${bin}/split-categorical2.py -f MC data.spec data.csv ch &&
  for c in ch*/; do
 	(cd $c &&
 	 ${bin}/split-categorical2.py -f Byte data.spec data.csv byte/ &&
