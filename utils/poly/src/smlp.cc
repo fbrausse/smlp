4b1cf62bce9279dad19af74adf8105893e9f3281
Franz Brauße <franz.brausse@manchester.ac.uk>
2022-10-25 13:43:01 -0400

    poly: add support for Not() for ALPHA, BETA

--- utils/poly/src/smlp.cc
+++ utils/poly/src/smlp.cc
@@ -306,16 +306,6 @@ static str smt2_logic_str(const domain &dom, const sptr<term2> &e)
 	return logic;
 }
 
-template <decltype(lbop2::op) op>
-static expr2s mk_lbop2(vec<expr2s> args)
-{
-	vec<sptr<form2>> b;
-	b.reserve(args.size());
-	for (expr2s &t : args)
-		b.emplace_back(move(*t.get<sptr<form2>>()));
-	return make2f(lbop2 { op, move(b) });
-}
-
 #ifdef SMLP_ENABLE_KERAS_NN
 # define USAGE "{ DOMAIN EXPR | H5-NN SPEC GEN IO-BOUNDS }"
 #else
@@ -426,11 +416,28 @@ static void version_info()
 #endif
 }
 
+template <decltype(lbop2::op) op>
+static expr2s mk_lbop2(vec<expr2s> args)
+{
+	vec<sptr<form2>> b;
+	b.reserve(args.size());
+	for (expr2s &t : args)
+		b.emplace_back(move(*t.get<sptr<form2>>()));
+	return make2f(lbop2 { op, move(b) });
+}
+
+static expr2s mk_lneg2(vec<expr2s> args)
+{
+	assert(size(args) == 1);
+	return make2f(lneg2 { move(*args.front().get<sptr<form2>>()) });
+}
+
 static sptr<form2> parse_infix_form2(const char *s)
 {
 	static const unroll_funs_t logic = {
 		{"And", mk_lbop2<lbop2::AND>},
-		{"Or", mk_lbop2<lbop2::OR>}
+		{"Or", mk_lbop2<lbop2::OR>},
+		{"Not", mk_lneg2},
 	};
 	return *unroll(parse_infix(s, false), logic).get<sptr<form2>>();
 }
