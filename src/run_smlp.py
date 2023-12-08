67fe9a8f46362850b14256b1b571325701684fc2
zurab khasidashvili <zurab.khasidashvili@intel.com>
2023-12-08 11:23:58 +0200

    top py script to run smlp

---
+++ src/run_smlp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#
+# This file is part of smlprover.
+#
+# Copyright 2019 Konstantin Korovin
+# Copyright 2020 Franz Brauße <franz.brausse@manchester.ac.uk>
+# See the LICENSE file for terms of distribution.
+
+# coding: utf-8
+
+
+import sys
+from smlp_py.smlp_flows import SmlpFlows
+
+
+def main(argv):
+    smlpInst = SmlpFlows(argv)
+    smlpInst.smlp_flow()
+
+if __name__ == "__main__":
+    main(sys.argv)
+    
