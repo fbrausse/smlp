dc3efdeb6eca52df885dda91c9c427fe488ab3c9
Dmitry Messerman <dmitry.messerman@gmail.com>
2026-05-23 12:43:18 +0300

    Removed redundant files

--- package_test/python3.12/almalinux_9/smlp_regr_patch.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-sed -i.bak 's@../../src@/usr/local/lib64/python3.12/site-packages/smlp@' /app/smlp/regr_smlp/code/smlp_regr.py
--- package_test/python3.12/opensuse_15.6/tkagg_patch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-sed -i.bak '/^from/iif os.path.exists("\/.dockerenv"): \
-    print("Script is running inside a Docker container.") \
-    import matplotlib \
-    matplotlib.use("TkAgg") \
-    import matplotlib.pyplot as plt\n' /usr/local/lib64/python3.12/site-packages/smlp/run_smlp.py
