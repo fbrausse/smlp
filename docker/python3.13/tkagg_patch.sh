5f18323e6e383ecb2a17323bc6912a5ad564b1ad
Dmitry Messerman <dmitry.messerman@gmail.com>
2026-05-23 12:52:31 +0300

    Removed redundant files [2]

--- docker/python3.13/tkagg_patch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-sed -i.bak '/^from/iif os.path.exists("\/.dockerenv"): \
-    print("Script is running inside a Docker container.") \
-    import matplotlib \
-    matplotlib.use("TkAgg") \
-    import matplotlib.pyplot as plt\n' /app/smlp/src/run_smlp.py
