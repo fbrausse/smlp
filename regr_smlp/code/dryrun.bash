817eebba924831a839a860b5320aca8cdeaf07e9
Dmitry Messerman <dmitry.messerman@gmail.com>
2026-06-09 09:23:27 +0300

    Preserving real run results during dryrun.bash execution

--- regr_smlp/code/dryrun.bash
+++ regr_smlp/code/dryrun.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export DRYRUN=1
 for t in $@; do
-    pytest -s test_cmdline.py::"Test${t}" |& egrep "^smlp|DRYRUN: smlp"
+    pytest --basetemp=/tmp/pytest-of-${USER}/dryrun -s test_cmdline.py::"Test${t}" |& egrep "^smlp|DRYRUN: smlp"
     if [[ $t != "${!#}" ]]; then
         printf '=%.0s' {1..80}
         echo ""
